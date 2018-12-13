94bc1628e500dc7f6e63bb4074307858fc811545
Alina Goaga <goaga.alina@gmail.com>
2018-12-13 16:13:54 +0000

    Added the attack method

---
+++ spec/features/user_stories_spec.rb
@@ -0,0 +1,25 @@
+feature 'Enter names' do
+  scenario 'submitting names' do
+    sign_in_and_play
+    expect(page).to have_content 'Dave vs. Mittens'
+  end
+end
+
+feature 'View hit points' do
+  scenario 'see Player 2 hit points' do
+    sign_in_and_play
+    expect(page).to have_content 'Mittens: 60HP'
+  end
+end
+
+feature 'Attacking' do
+  scenario 'attack Player 2' do
+    sign_in_and_play
+    click_link 'Attack'
+    expect(page).to have_content 'Dave attacked Mittens'
+  end
+end
+
+# As Player 1,
+# So I can start to win a game of Battle,
+# I want my attack to reduce Player 2's HP
